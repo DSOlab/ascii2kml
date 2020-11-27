8549f19ece9f777607930e7a646fef2d15a4d82c
xanthos <xanthos.papanikolaou@gmail.com>
2020-11-27 20:58:51 +0200

    added spaces in names

--- ascii2kml.py
+++ ascii2kml.py
@@ -5,7 +5,68 @@ from __future__ import print_function
 import sys
 import argparse
 
-def printer(*msg): print(*msg, file = sys.stderr) 
+def printer(*msg): print(*msg, file = sys.stderr)
+
+def is_float_with_dot(str):
+    try:
+        float(str)
+        return "." in str
+    except:
+        return False
+
+class Placemark:
+    
+    def __init__(self, lat, lon, name="", description=""):
+        self.latitude = lat
+        self.longtitude = lon
+        self.name = name
+        self.description = description
+    
+    def flush(self):
+        print("  <Placemark>")
+        print("    <name>{:}</name>".format(self.name))
+        print("    <description>{:}</description>".format(self.description))
+        print("    <Point><coordinates>{:},{:},0.0</coordinates></Point>".format(self.longtitude, self.latitude))
+        print("  </Placemark>")
+        return
+
+def parse_line(line, names_w_spaces=False, description_col=None):
+    l = line.split()
+    if len(l) < 3:
+        ermsg = "[ERROR] Number of fields in line \"{:}\" < 3".format(line)
+        printer(ermsg)
+        raise RuntimeError(ermsg)
+    ##  resolve point name
+    if names_w_spaces:
+        lat_str = ""
+        for idx, colstr in enumerate(l):
+            if is_float_with_dot(colstr):
+                lat_str = colstr
+                break
+        if idx > len(l) - 2:
+            ermsg = "[ERROR] Failed to resolve line \"{:}\". Cannot find lat/lon after name".format(line)
+            printer(ermsg)
+            raise RuntimeError(ermsg)
+        name = line[0:line.find("{:}".format(lat_str))-1]
+    else:
+        name = l[0]
+        idx = 0
+    ##  resolve latitude/longtitude
+    assert(len(l)>=idx+2)
+    try:
+      lat, lon = [ float(x) for x in [l[idx], l[idx+1]] ]
+    except:
+          ermsg = "[ERROR] Failed to resolve lat/lon fields in line \"{:}\"".format(line)
+          printer(ermsg)
+          raise RuntimeError(ermsg)
+    ##  resolve description
+    if description_col and len(l) > description_col+idx:
+      description_col += idx-1
+      description = line[line.find(" {:} ".format(l[description_col]))+1:].rstrip()
+    else:
+      description = ""
+    ##  return Placemark
+    return Placemark(lat, lon, name, description)
 
 def flush_placemark(lat, lon, name="", description=""):
     print("  <Placemark>")
@@ -33,7 +94,7 @@ class myFormatter(
 
 parser = argparse.ArgumentParser(
     formatter_class=myFormatter,
-    description='Ascii files to KML format',
+    description='Read in points (on the earth\'s surface) and transform them to Placemarks in a valid KML document.',
     epilog=('''National Technical University of Athens,
       Dionysos Satellite Observatory\n
       Send bug reports to:
@@ -47,6 +108,12 @@ parser.add_argument('--document-name',
     dest='doc_name',
     required=False,
     help='The name of the \"Document\" tag, inside the kml file (note that this is not the output filename).')
+
+parser.add_argument('--name-w-spaces',
+    #metavar='NAMES_INCLUDE_WHITESPACES',
+    dest='names_w_spaces',
+    action='store_true',
+    help='If set then the station/point names can include whitespaces. That means, that the name of each point will start from the begining of the line and stop untill we reach a column that can be interpreted as float (i.e. the latitude).')
 
 parser.add_argument('--description-after-col',
     default=None,
@@ -65,19 +132,6 @@ if args.description_col: assert(args.description_col>=3)
 open_kml(args.doc_name)
 for line in sys.stdin:
     if not line.lstrip().startswith('#'):
-        l = line.split()
-        if len(l) < 3:
-            printer("[ERROR] Number of fields in line \"{:}\" < 3".format(line))
-            sys.exit(1)
-        name = l[0]
-        try:
-          lat, lon = [ float(x) for x in [l[1], l[2]] ]
-        except:
-            printer("[ERROR] Failed to resolve lat/lon fields in line \"{:}\"".format(line))
-            sys.exit(1)
-        if args.description_col  and len(l) > args.description_col:
-          description = line[line.find(" {:} ".format(l[args.description_col]))+1:]
-        else:
-          description = ""
-        flush_placemark(lat, lon, name, description)
+        placemark = parse_line(line, args.names_w_spaces, args.description_col)
+        placemark.flush()
 close_kml()
